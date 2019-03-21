b8836f0463f509a3d0537a36e6f136bb1393acb5
Alain Lotissier <aslotissier@uc.cl>
2019-03-21 16:46:33 -0300

    spaceships made

--- app/controllers/starships_controller.rb
+++ app/controllers/starships_controller.rb
@@ -1,2 +1,20 @@
 class StarshipsController < ApplicationController
+  require 'rest-client'
+  def index
+    @name = params[:name]
+    @response = RestClient.get('https://swapi.co/api/starships/' + @name);
+    @jsonResponse = JSON.parse(@response.body);
+    
+
+    @pilots = {};
+    @films = {};
+
+    @jsonResponse['films'].each do |films|
+      @films[films.to_s] = JSON.parse(RestClient.get(films).body)['title']
+    end
+
+    @jsonResponse['pilots'].each do |pilots|
+      @pilots[pilots.to_s] = JSON.parse(RestClient.get(pilots).body)['name']
+    end
+  end
 end
