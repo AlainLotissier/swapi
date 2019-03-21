b594ed48df53bcd6f4b1e8200697f94a4cd17e7f
Alain Lotissier <aslotissier@uc.cl>
2019-03-21 16:31:46 -0300

    characters made

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,5 +1,21 @@
 class CharactersController < ApplicationController
+  require 'rest-client'
   def index
+    @name = params[:name]
+    @response = RestClient.get('https://swapi.co/api/people/' + @name);
+    @jsonResponse = JSON.parse(@response.body);
+    @homeworld = JSON.parse(RestClient.get(@jsonResponse['homeworld']).body)['name']
+
+    @films = {};
+    @starships = {};
+
+    @jsonResponse['films'].each do |films|
+      @films[films.to_s] = JSON.parse(RestClient.get(films).body)['title']
+    end
+
+    @jsonResponse['starships'].each do |starships|
+      @starships[starships.to_s] = JSON.parse(RestClient.get(starships).body)['name']
+    end
   end
 
   def show
--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -6,21 +6,21 @@ class MovieController < ApplicationController
     @id = params[:id]
     @response = RestClient.get('https://swapi.co/api/films/' + @id);
     @jsonResponse = JSON.parse(@response.body);
-    @characters = [];
-    @planets = [];
-    @starships = [];
+    @characters = {};
+    @planets = {};
+    @starships = {};
 
     @jsonResponse['characters'].each do |characters|
-      @characters.push(JSON.parse(RestClient.get(characters).body)['name'])
+      @characters[characters.to_s] = JSON.parse(RestClient.get(characters).body)['name']
     end
 
     @jsonResponse['planets'].each do |planets|
-      @planets.push(JSON.parse(RestClient.get(planets).body)['name'])
+      @planets[planets.to_s] = JSON.parse(RestClient.get(planets).body)['name']
     end
 
     @jsonResponse['starships'].each do |starships|
-      @starships.push(JSON.parse(RestClient.get(starships).body)['name'])
+      @starships[starships.to_s] = JSON.parse(RestClient.get(starships).body)['name']
     end
-    
+
   end
 end
