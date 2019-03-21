a6525ebe858818995e2394160667e959c30890cc
Alain Lotissier <aslotissier@uc.cl>
2019-03-21 16:38:59 -0300

    planets made

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,2 +1,20 @@
 class PlanetsController < ApplicationController
+  require 'rest-client'
+  def index
+    @name = params[:name]
+    @response = RestClient.get('https://swapi.co/api/planets/' + @name);
+    @jsonResponse = JSON.parse(@response.body);
+
+
+    @residents = {};
+    @films = {};
+
+    @jsonResponse['films'].each do |films|
+      @films[films.to_s] = JSON.parse(RestClient.get(films).body)['title']
+    end
+
+    @jsonResponse['residents'].each do |residents|
+      @residents[residents.to_s] = JSON.parse(RestClient.get(residents).body)['name']
+    end
+  end
 end
