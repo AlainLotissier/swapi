64e85d1dce02a3d182230bd458f3ba221eff3ad6
Alain Lotissier <aslotissier@uc.cl>
2019-03-20 22:45:49 -0300

    test 2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   require 'rest-client'
-  @response = RestClient.get 'https://swapi.co/api/films'
+  @response = RestClient.get 'https://swapi.co/api/films';
+  @jsonResponse = JSON.parse(@response.body);
 end
